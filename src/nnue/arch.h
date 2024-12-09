0e009e8abcffdf2e86d8000bbd70b613e0f71f77
Liam McGuire <liam.t.mcguire@gmail.com>
2024-12-08 23:34:51 -0500

    bench: 5064107

--- src/nnue/arch.h
+++ src/nnue/arch.h
@@ -2,6 +2,7 @@
 
 #include <immintrin.h>
 #include "../defs.h"
+#include "simd.h"
 
 constexpr auto INPUT_BUCKETS = 14;
 constexpr auto INPUT_SIZE = 768;
@@ -15,19 +16,21 @@ constexpr auto FT_SHIFT = 10;
 constexpr auto L1_QUANT = 64;
 constexpr auto OutputScale = 400;
 
-constexpr auto U8_CHUNK_SIZE = sizeof(__m256i) / sizeof(u8);
-constexpr auto I16_CHUNK_SIZE = sizeof(__m256i) / sizeof(i16);
-constexpr auto I32_CHUNK_SIZE = sizeof(__m256i) / sizeof(i32);
-constexpr auto F32_CHUNK_SIZE = sizeof(__m256i) / sizeof(float);
+constexpr auto U8_CHUNK_SIZE = sizeof(vec_i8) / sizeof(u8);
+constexpr auto I16_CHUNK_SIZE = sizeof(vec_i16) / sizeof(i16);
+constexpr auto I32_CHUNK_SIZE = sizeof(vec_i32) / sizeof(i32);
+constexpr auto F32_CHUNK_SIZE = sizeof(vec_ps) / sizeof(float);
 
-constexpr auto NNZ_INPUT_SIMD_WIDTH = sizeof(__m256i) / sizeof(i32);
+constexpr auto NNZ_INPUT_SIMD_WIDTH = sizeof(vec_i32) / sizeof(i32);
 constexpr auto NNZ_CHUNK_SIZE = (NNZ_INPUT_SIMD_WIDTH > 8) ? NNZ_INPUT_SIMD_WIDTH : 8;
 constexpr auto NNZ_OUTPUTS_PER_CHUNK = NNZ_CHUNK_SIZE / 8;
 
 constexpr auto L1_CHUNK_PER_32 = sizeof(i32) / sizeof(i8);
 constexpr auto L1_PAIR_COUNT = L1_SIZE / 2;
 
-constexpr auto SIMD_CHUNKS = L1_SIZE / (sizeof(__m256i) / sizeof(i16));
+constexpr auto SIMD_CHUNKS = L1_SIZE / (sizeof(vec_i16) / sizeof(i16));
+
+constexpr float L1_MUL = (1 << FT_SHIFT) / static_cast<float>(FT_QUANT * FT_QUANT * L1_QUANT);
 
 constexpr auto N_FTW = INPUT_SIZE * L1_SIZE * INPUT_BUCKETS;
 constexpr auto N_FTB = L1_SIZE;
