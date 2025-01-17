cbff37d643d6ef9edecea2e34343a27335e5320a
Liam McGuire <12057628+liamt19@users.noreply.github.com>
2025-01-17 15:45:04 -0500

    Added branch tracking (#10)
    bench: 5057108

---
+++ src/util/dbg_hit.h
@@ -0,0 +1,21 @@
+
+#pragma once
+
+#ifndef DBG_HIT_H
+#define DBG_HIT_H 1
+
+#include "../types.h"
+
+namespace Horsie {
+	void dbg_hit_on(bool cond, int slot = 0);
+	void dbg_mean_of(int64_t value, int slot = 0);
+	void dbg_stdev_of(int64_t value, int slot = 0);
+	void dbg_extremes_of(int64_t value, int slot = 0);
+	void dbg_correl_of(int64_t value1, int64_t value2, int slot = 0);
+	void dbg_print();
+}
+
+#endif // !DBG_HIT_H
+
+
+
