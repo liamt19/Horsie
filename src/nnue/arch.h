a0d4e1654887a93a9d595254d62dc9791cc88d3a
Liam McGuire <liam.t.mcguire@gmail.com>
2025-02-23 00:18:52 -0500

    sb 560, bench: 4774524

--- src/nnue/arch.h
+++ src/nnue/arch.h
@@ -6,8 +6,8 @@
 constexpr auto INPUT_BUCKETS = 10;
 constexpr auto INPUT_SIZE = 704;
 constexpr auto L1_SIZE = 1536;
-constexpr auto L2_SIZE = 16;
-constexpr auto L3_SIZE = 32;
+constexpr auto L2_SIZE = 32;
+constexpr auto L3_SIZE = 64;
 constexpr auto L3_HALF_SIZE = L3_SIZE / 2;
 constexpr auto OUTPUT_BUCKETS = 8;
 
