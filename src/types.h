55aa639902410c5a7b46ed46062a609643625a9c
Liam McGuire <liam.t.mcguire@gmail.com>
2025-05-09 10:46:18 -0400

    version bench: 4771861

--- src/types.h
+++ src/types.h
@@ -23,7 +23,7 @@
 namespace Horsie {
 
     constexpr auto InitialFEN = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1";
-    constexpr auto EngVersion = "1.0.21";
+    constexpr auto EngVersion = "1.0.22";
 
     constexpr u64 FileABB = 0x0101010101010101ULL;
     constexpr u64 FileBBB = FileABB << 1;
